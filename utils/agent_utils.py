a0bd8d3c67e02ca6701b96ed48c6edb6d4d535e0
Clement Apavou <apavou.clement@outlook.fr>
2022-02-26 14:50:23 +0100

    remove useless file

--- utils/agent_utils.py
+++ utils/agent_utils.py
@@ -4,6 +4,9 @@ import os
 import wandb
 from config.hparams import Parameters
 from Datasets.datamodule import BaseDataModule
+from rich.progress import (BarColumn, Progress, SpinnerColumn, TextColumn,
+                           TimeElapsedColumn, TimeRemainingColumn)
+
 
 def get_net(network_name, network_param):
     """
@@ -13,7 +16,6 @@ def get_net(network_name, network_param):
     mod = importlib.import_module(f"models.{network_name}")
     net = getattr(mod,network_name)
     return net(network_param)
-
 
 def get_artifact(name: str, type: str) -> str:
     """Artifact utilities
@@ -27,7 +29,6 @@ def get_artifact(name: str, type: str) -> str:
         return file_path
     else: 
         return None
-    
 
 def get_datamodule(data_param):
     """
@@ -35,10 +36,21 @@ def get_datamodule(data_param):
     """
     return BaseDataModule(data_param)
 
-
 def parse_params(parameters: Parameters) -> dict:
     wdb_config = {}
     for k,v in vars(parameters).items():
         for key,value in vars(v).items():
             wdb_config[f"{k}-{key}"]=value
     return wdb_config
+
+def get_progress_bar():
+    return Progress(
+        "[progress.description]{task.description}",
+        SpinnerColumn(),
+        BarColumn(),
+        "[progress.percentage]{task.percentage:>3.0f}%",
+        TextColumn("[bold blue]{task.fields[info]}", justify="right"),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+        "\n"
+    )
